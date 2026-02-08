9587134e9f99a89c75e5d60d36e55bf46b23a722
mehmetsefaciftci <mehmet.ciftci@sabanciuniv.edu>
2026-02-08 20:57:19 +0300

    chore: remove node_modules from repo

---
+++ android/app/src/main/java/com/labyrinth/mazegame/MainActivity.java
@@ -0,0 +1,5 @@
+package com.labyrinth.mazegame;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.labyrinth.mazegame',
+  appName: 'Maze game mobile design',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
