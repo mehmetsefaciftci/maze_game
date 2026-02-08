0e4e7391e2b605d865eda9c1c2887c9874f58a2f
mehmetsefaciftci <mehmet.ciftci@sabanciuniv.edu>
2026-02-09 02:29:44 +0300

    updates

--- src/services/userStore.ts
+++ src/services/userStore.ts
@@ -1,4 +1,4 @@
-import { db } from "../firebase";
+import { db } from "./firebase";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 
 export async function upsertUser(uid: string) {
