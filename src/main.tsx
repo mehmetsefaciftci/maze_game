489f1093ad7fa08669a55c08e6244179ad2bbaa9
mehmetsefaciftci <mehmet.ciftci@sabanciuniv.edu>
2026-02-10 04:38:44 +0300

    updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,10 @@
-import { doc, getDoc, serverTimestamp, setDoc, updateDoc } from "firebase/firestore";
-import { db } from "./services/firebase";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-/**
- * users/{uid} dokümanını:
- * - yoksa oluşturur
- * - varsa lastSeenAt günceller
- */
-export async function upsertUser(uid: string) {
-  const ref = doc(db, "users", uid);
-  const snap = await getDoc(ref);
-
-  if (!snap.exists()) {
-    await setDoc(ref, {
-      createdAt: serverTimestamp(),
-      lastSeenAt: serverTimestamp(),
-      currentLevel: 1,
-      completedLevels: [],
-    });
-  } else {
-    await updateDoc(ref, {
-      lastSeenAt: serverTimestamp(),
-    });
-  }
-
-  return ref;
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
